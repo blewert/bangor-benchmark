840dfc504dce8d2313099451580eaf8c2eb2fb68
ben <eeu222@bangor.ac.uk>
2015-08-14 14:26:56 +0100

    started working on xml parser

---
+++ Assets/Scripts/Startup Screen/Mode.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mode
+{
+	public string name;
+	public List<Mode.Entry> supportedEnvironments;
+	public List<Mode.Entry> supportedCharacters;
+	
+	public void setName(string name)
+	{
+		this.name = name;
+	}
+	
+	public void setSupportedEnvironments(List<Mode.Entry> supportedEnvironments)
+	{
+		this.supportedEnvironments = supportedEnvironments;
+	}
+	
+	public void setSupportedCharacters(List<Mode.Entry> supportedCharacters)
+	{
+		this.supportedCharacters = supportedCharacters;
+	}
+	
+	public class Entry
+	{
+		public string name;
+		public string path;
+		
+		public Entry(string name, string path)
+		{
+			this.name = name;
+			this.path = path;
+		}
+	}
+}
---
+++ Assets/Scripts/Startup Screen/ModeConfigurationParser.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+
+
+public class ModeConfigurationParser : MonoBehaviour 
+{
+	public string pathToXMLFile; 
+	private List<Mode> modes = new List<Mode>();
+	
+	// Use this for initialization
+	void Start ()
+	{
+		if(!File.Exists(pathToXMLFile))
+		{
+			Debug.LogError("XML file not found: " + pathToXMLFile);
+			return;
+		}
+		
+		string contents = File.ReadAllText(pathToXMLFile);
+		parseXML(contents);
+	}
+	
+	
+	private void parseXML(string contents)
+	{
+		XElement root = XElement.Load (pathToXMLFile);
+		
+		foreach(var mode in root.Elements ("mode"))
+		{
+			var gamemode = new Mode();
+			
+			foreach(var elem in mode.Elements ())
+			{
+				if(elem.Name == "name")
+					gamemode.setName(elem.Value);
+				
+				else if(elem.Name == "supportedEnvironments")
+				{
+					List<Mode.Entry> environments = new List<Mode.Entry>();
+					
+					foreach(var environment in elem.Elements("environment"))
+					{
+						string name = environment.Element("name").Value;
+						string path = environment.Element("path").Value;
+						
+						environments.Add (new Mode.Entry(name, path));
+					}
+					
+					gamemode.setSupportedEnvironments(environments);	
+				}
+				
+				else if(elem.Name == "supportedCharacters")
+				{
+					List<Mode.Entry> characters = new List<Mode.Entry>();
+					
+					foreach(var character in elem.Elements("character"))
+					{
+						string name = character.Element("name").Value;
+						string path = character.Element("path").Value;
+						
+						characters.Add (new Mode.Entry(name, path));
+					}
+					
+					gamemode.setSupportedCharacters(characters);	
+				}
+			}
+			
+			modes.Add (gamemode);
+		}
+		
+		foreach(var env in modes[0].supportedEnvironments)
+		{
+			print (env.name + " -> "+ env.path);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

458e0006c9a223c0f8363c4c8203b1fb48de3b61
JamesJacko <james.ogge@gmail.com>
2015-10-08 09:30:17 +0100

    tank

---
+++ Assets/Scripts/Controllers/TankHumanController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankHumanController : MonoBehaviour {
+
+	public ITankNPC npc;
+	int i = 0;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		npc = GetComponent<ITankNPC>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			npc.turnLeft();
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			npc.turnRight();
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			npc.moveForward();
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			npc.moveBackward();
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			npc.TurnTurretLeft ();
+		} else if (Input.GetKey (KeyCode.D)) {
+			npc.TurnTurretRight ();
+		}
+//		if (Input.GetKey (KeyCode.X)){
+//			Debug.Log ("pressed " + i);
+//			var rigidBody = npc.gameObject.AddComponent<Rigidbody>();
+//			rigidBody.AddExplosionForce(1000.0f, Vector3.zero, 1000.0f, 1000.0f, ForceMode.Impulse);
+//			i++;
+//		}
+	}
+}
---
+++ Assets/Scripts/Interfaces/NPC/ITankNPC.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ITankNPC : INPC {
+	abstract public void TurnTurretLeft();
+
+	abstract public void TurnTurretRight();
+}

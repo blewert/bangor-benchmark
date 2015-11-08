ed1cddd89e53ff78b456f303a9811e4f55b7aa38
Benjamin Williams <eeu222@bangor.ac.uk>
2015-11-08 02:27:54 +0000

    started work on dynamic menu. functionality works, but saving information is needed
    Signed-off-by: Benjamin Williams <eeu222@bangor.ac.uk>

--- Assets/Scripts/Bootup.cs
+++ Assets/Scripts/Bootup.cs
@@ -1,16 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Linq;
 
+/// <summary>
+/// The start-up script for loading dynamically loading in primitives/instances; provides logic for MenuHandler script.
+/// </summary>
 public class Bootup : MonoBehaviour 
 {
+	//The menu object and it's MenuHandler component
+	[Header("Bootup menu options")]
+	public GameObject menu;
+	private MenuHandler menuComponent;
+	
+	//Environment primitives and instances
+	private EnvironmentPrimitive[] environmentPrimitives;
+	private EnvironmentInstance[] environmentInstances;
+	
+	
 	// Use this for initialization
 	void Start () 
 	{
-		var primitives = PrimitivesParser.getEnvironmentPrimitives();
-		var instances = PrimitivesParser.getEnvironmentInstances();
+		//Get all environment primitives and instances
+		environmentPrimitives = PrimitivesParser.getEnvironmentPrimitives();
+		environmentInstances  = PrimitivesParser.getEnvironmentInstances();
+
+		//Does the menu object exist?
+		if(menu == null)
+			throw new UnityException("The menu object which has been specified is null.");
+			
+		//If so, get it's menu handler script (for the UI changing functionality)
+		menuComponent = menu.GetComponent<MenuHandler>();
 		
-		foreach(var primitive in primitives)
+		//Does this exist?
+		if(menuComponent == null)
+			throw new UnityException("Could not find attached MenuHandler script in menu object.");
+		
+		//Do the left/right buttons exist?
+		if(menuComponent.leftButton == null || menuComponent.rightButton == null)
+			throw new UnityException("Left/right navigation buttons for MenuHandler script are not set.");
+		
+		//If everything is fine, register left/right button handlers and hook view update event
+		menuComponent.registerNavigationButtonHandlers(this);
+		menuComponent.onViewUpdate += onViewUpdate;
+		
+		//Add the initial primitive buttons (initially the environment buttons)
+		menuComponent.addPrimitivesButtons(environmentPrimitives.Select(x => x.name), this);
+		
+		/*foreach(var primitive in primitives)
 		{
 			var linkedInstances = instances.Where (x => primitive.name == x.primitiveName);
 			
@@ -21,8 +58,73 @@ public class Bootup : MonoBehaviour
 		}
 		
 		var environment = gameObject.AddComponent<ForestPrimitive>();
-		environment.instance = instances[0]; //change to selected value
+		environment.instance = instances[0]; //change to selected value*/
 		
+	} 
+	
+	/// <summary>
+	/// Callback when a primitives button is clicked in the menu. This could be an environment primitive button, a gamemode primitive button or a character primitive button.
+	/// </summary>
+	/// <param name="button">The button which was clicked.</param>
+	/// <param name="buttonText">The text of the button.</param>
+	public void onPrimitivesButtonClick(Button button, string buttonText)
+	{
+		//Debug.Log ("Primitive button clicked: " + buttonText);
+		
+		var associatedInstances = environmentInstances.Where(x => x.primitiveName.Equals(buttonText));
+		
+		menuComponent.removeInstanceButtons();
+		menuComponent.addInstancesButtons(associatedInstances.Select (x => x.name), this);		
 	}
-
+	
+	/// <summary>
+	/// Callback when a instances button is clicked in the menu. This could be an environment instance button, a gamemode instance button or a character instance button.
+	/// </summary>
+	/// <param name="button">The button which was clicked.</param>
+	/// <param name="buttonText">The text of the button.</param>
+	public void onInstancesButtonClick(Button button, string buttonText)
+	{
+		//Debug.Log ("Instance button clicked: " + buttonText);
+	}
+	
+	/// <summary>
+	/// Called when the left arrow of the menu is clicked.
+	/// </summary>
+	public void onLeftArrowClick()
+	{	
+		menuComponent.moveView (-1);
+	}
+	
+	/// <summary>
+	/// Called when the right arrow of the menu is clicked.
+	/// </summary>
+	public void onRightArrowClick()
+	{	
+		menuComponent.moveView (+1);
+	}
+	
+	/// <summary>
+	/// Called when the view has updated from it's old value.
+	/// </summary>
+	/// <param name="newView">For convenience, the new view value</param>
+	public void onViewUpdate(MenuHandler.View view)
+	{
+		//At this point, all the buttons in the menu have been deleted.
+		//..
+		
+		if(view == MenuHandler.View.ENVIRONMENT)
+		{
+			//Environment
+			menuComponent.addPrimitivesButtons(environmentPrimitives.Select (x => x.name), this);
+		}
+		else if(view == MenuHandler.View.GAMEMODE)
+		{
+			//Add gamemode buttons...
+		}
+		else if(view == MenuHandler.View.CHARACTER)
+		{
+			//Add character buttons...
+		}
+	}
+	
 }
---
+++ Assets/Scripts/PrimitivesMenu/MenuHandler.cs
@@ -0,0 +1,290 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Menu handler class for the start-up primitives/instances selection menu. Performs UI interaction and provides events/callbacks 
+/// to be hooked by the start-up script.
+/// </summary>
+public class MenuHandler : MonoBehaviour
+{
+	[Header("Menu header labels")]
+	public RectTransform primitivesMenuLabel;
+	public RectTransform instancesMenuLabel;
+	
+	[Header("Navigation buttons")]
+	public Button leftButton;
+	public Button rightButton;
+	
+	[Header("Button options")]
+	public Button buttonPrefab;
+	public float buttonPadding;
+	
+	//The two types of buttons/columns in the menu
+	public enum MenuButtonType
+	{
+		PRIMITIVES, INSTANCES
+	};
+	
+	//Lists of the primitive and instance buttons for usage by other scripts
+	private List<Button> primitiveButtons = new List<Button>();
+	private List<Button> instanceButtons = new List<Button>();
+			
+	//The possible view options
+	public enum View
+	{
+		ENVIRONMENT, GAMEMODE, CHARACTER
+	}
+	
+	//The current "view" and view index - which type of menu is showing? characters, environment, gamemodes?
+	private View currentView;
+	private int currentViewIndex;
+	
+	//Event to hook when the view is updated.
+	public delegate void ViewUpdateHandler(View view);
+	public event ViewUpdateHandler onViewUpdate;
+	
+	
+	/// <summary>
+	/// Gets the header label for either the primitives or instances column.
+	/// </summary>
+	/// <returns>The relevant header label, as an object.</returns>
+	/// <param name="buttonType">The type of button grouped in this column (either primitives or instances).</param>
+	public RectTransform getHeaderLabel(MenuButtonType buttonType)
+	{
+		if(buttonType == MenuButtonType.PRIMITIVES)
+			return primitivesMenuLabel;
+		
+		else if(buttonType == MenuButtonType.INSTANCES)
+			return instancesMenuLabel;
+		
+		return null;
+	}
+	
+	
+	/// <summary>
+	/// Sets up click callbacks for the navigation buttons, from a referring script.
+	/// </summary>
+	/// <param name="referrer">The script which wishes to hook the navigation buttons click event.</param>
+	public void registerNavigationButtonHandlers(Bootup referrer)
+	{
+		//Left button
+		leftButton.onClick.AddListener(delegate
+		{
+			referrer.onLeftArrowClick();		
+		});
+		
+		//Right button
+		rightButton.onClick.AddListener(delegate
+		{
+			referrer.onRightArrowClick();		
+		});
+	}
+	
+	
+	/// <summary>
+	/// Adds a number of primitive buttons underneath the correct header. A button is created and it's text is set for every string passed into this function.
+	/// </summary>
+	/// <param name="strs">The strings for each button.</param>
+	/// <param name="referrer">The referring script to hook the each button's callback.</param>
+	public void addPrimitivesButtons(IEnumerable<string> strs, Bootup referrer)
+	{
+		//Just call the array method by converting IEnumerable to an array
+		addPrimitivesButtons(strs.ToArray(), referrer);
+	}
+
+
+	/// <summary>
+	/// Adds a number of instance buttons underneath the correct header. A button is created and it's text is set for every string passed into this function.
+	/// </summary>
+	/// <param name="strs">The strings for each button.</param>
+	/// <param name="referrer">The referring script to hook the each button's callback.</param>
+	public void addInstancesButtons(IEnumerable<string> strs, Bootup referrer)
+	{
+		//Just call the array method by converting IEnumerable to an array
+		addInstancesButtons(strs.ToArray(), referrer);
+	}
+	
+	
+	/// <summary>
+	/// Adds a number of primitive buttons underneath the correct header. A button is created and it's text is set for every string passed into this function.
+	/// </summary>
+	/// <param name="strs">The strings for each button.</param>
+	/// <param name="referrer">The referring script to hook the each button's callback.</param>
+	public void addPrimitivesButtons(string[] buttonTexts, Bootup referrer)
+	{
+		//Get the start label to offset each button from, and find the starting position.
+		var startLabel = getHeaderLabel(MenuButtonType.PRIMITIVES);
+		Vector3 startPosition = startLabel.transform.position;
+		
+		for(int i = 0; i < buttonTexts.Length; i++)
+		{
+			//Run through each button string, and instantiate a button for this string.
+			var button = (Button)Instantiate(buttonPrefab, startPosition, Quaternion.identity);
+			
+			//Set this menu as the button's root, and set the text of the button to this string.
+			button.transform.SetParent (transform);
+			button.GetComponentInChildren<Text>().text = buttonTexts[i];
+			
+			//Get the button's position, height and the height of the starting label
+			var position = button.transform.position;
+			var height = button.GetComponent<RectTransform>().rect.height;
+			var startLabelHeight = startLabel.rect.height;
+			
+			//Calculate this button's position:
+			//Start label height + offset + (the amount of buttons so far, combined height) + padding
+			position.y -= (startLabelHeight + buttonPadding) + (height * i) + buttonPadding;
+			
+			//Hook the callback
+			button.onClick.AddListener(delegate
+			{ 
+				referrer.onPrimitivesButtonClick(button, button.GetComponentInChildren<Text>().text);
+			});
+			
+			//Finally, set the button's position properly and add it to the list of primitive buttons.
+			button.transform.position = position;	
+			primitiveButtons.Add(button);
+		}
+	}
+	
+	
+	/// <summary>
+	/// Adds a number of instance buttons underneath the correct header. A button is created and it's text is set for every string passed into this function.
+	/// </summary>
+	/// <param name="strs">The strings for each button.</param>
+	/// <param name="referrer">The referring script to hook the each button's callback.</param>
+	public void addInstancesButtons(string[] buttonTexts, Bootup referrer)
+	{
+		//Get the start label to offset each button from, and find the starting position.
+		var startLabel = getHeaderLabel(MenuButtonType.INSTANCES);
+		Vector3 startPosition = startLabel.transform.position;
+			
+		for(int i = 0; i < buttonTexts.Length; i++)
+		{
+			//Run through each button string, and instantiate a button for this string.
+			var button = (Button)Instantiate(buttonPrefab, startPosition, Quaternion.identity);
+			
+			//Set this menu as the button's root, and set the text of the button to this stri
+			button.transform.SetParent (transform);
+			button.GetComponentInChildren<Text>().text = buttonTexts[i];
+			
+			//Get the button's position, height and the height of the starting label
+			var position = button.transform.position;
+			var height = button.GetComponent<RectTransform>().rect.height;
+			var startLabelHeight = startLabel.rect.height;
+			
+			//Calculate this button's position:
+			//Start label height + offset + (the amount of buttons so far, combined height) + padding
+			position.y -= (startLabelHeight + buttonPadding) + (height * i) + buttonPadding;
+			
+			//Hook the callback
+			button.onClick.AddListener(delegate
+			{ 
+				referrer.onInstancesButtonClick(button, button.GetComponentInChildren<Text>().text);
+			});
+			
+			//Finally, set the button's position properly and add it to the list of primitive buttons.
+			button.transform.position = position;	
+			instanceButtons.Add(button);
+		}
+	}
+	
+	
+	/// <summary>
+	/// Removes all primitive buttons.
+	/// </summary>
+	public void removePrimitiveButtons()
+	{
+		//Destroy all buttons
+		for(int i = 0; i < primitiveButtons.Count; i++)
+			Destroy (primitiveButtons[i].gameObject);
+		
+		//Clear list
+		primitiveButtons.Clear ();
+	}
+	
+	
+	/// <summary>
+	/// Removes all instance buttons.
+	/// </summary>
+	public void removeInstanceButtons()
+	{
+		//Destroy all buttons
+		for(int i = 0; i < instanceButtons.Count; i++)
+			Destroy (instanceButtons[i].gameObject);
+		
+		//Clear list
+		instanceButtons.Clear ();
+	}
+	
+	
+	/// <summary>
+	/// Gives a view enum type from a specific index.
+	/// </summary>
+	/// <returns>A enum type (View) for this specific index.</returns>
+	/// <param name="index">The index into the enum.</param>
+	private View getViewFromIndex(int index)
+	{
+		return (new View[] { View.ENVIRONMENT, View.GAMEMODE, View.CHARACTER })[index];
+	}
+	
+	
+	/// <summary>
+	/// Gives a header label string from a specific index.
+	/// </summary>
+	/// <returns>A string for a header label, for this specific index.</returns>
+	/// <param name="index">The index into the string array.</param>
+	private string getHeaderFromIndex(int index)
+	{
+		return (new string[] { "Environment", "Gamemode", "Character" })[index];
+	}
+	
+	
+	/// <summary>
+	/// Gets the current view as an enum type.
+	/// </summary>
+	/// <returns>The current view.</returns>
+	public View getCurrentView()
+	{
+		return currentView;
+	}
+	
+	
+	/// <summary>
+	/// Updates the view, and the entire menu system, also invokes onUpdateView callback
+	/// </summary>
+	private void updateView()
+	{
+		//Remove buttons
+		removeInstanceButtons();
+		removePrimitiveButtons();
+		
+		//Update headers
+		primitivesMenuLabel.GetComponentInChildren<Text>().text = getHeaderFromIndex(currentViewIndex) + " Primitives";
+		instancesMenuLabel.GetComponentInChildren<Text>().text  = getHeaderFromIndex(currentViewIndex) + " Instances";
+		
+		//Call event to listeners
+		onViewUpdate.Invoke(currentView);
+	}
+	
+	
+	/// <summary>
+	/// Changes the current view depending on whether or not it exceeds the number of possible views.
+	/// </summary>
+	/// <param name="movement">The movement: -ve values go left, +ve values go right</param>
+	public void moveView(int movement)
+	{
+		//The index hasn't changed. We don't need to move or update.
+		if(Mathf.Clamp (currentViewIndex + movement, 0, 2) == currentViewIndex)
+			return;
+		
+		//Get next view to display, and update view index for the next click.
+		currentView = getViewFromIndex(Mathf.Clamp (currentViewIndex + movement, 0, 2));
+		currentViewIndex += movement;
+		
+		//Update the view if something has changed
+		updateView();
+	}
+}

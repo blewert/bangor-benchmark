b0bb22a7e94cb6976e61b63dc966e096e2eef7e2
JamesJacko <james.ogge@gmail.com>
2015-11-19 13:25:10 +0000

    new branch

---
+++ TODO.js
@@ -0,0 +1,20 @@
+Issues,
+
+  Who's firing
+    Fire particle system or line trace
+    Empty Game Object
+  Player Targeting
+    Fire fan / sphere cast
+  Off Island
+    Island Rectangle Collider
+  Indicators
+
+GetGameObjectsByTag
+
+
+
+Bigger particles
+Longer Shot distance
+companion
+Respawn after 10 kills
+Save Data.

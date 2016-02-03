61c3cc3ffb653371d4154f8b7976eebdde4d9b19
ben <eeu222@bangor.ac.uk>
2016-02-03 15:16:26 +0000

    added basic client scene

--- Assets/Scripts/Networking/NetworkServer.cs
+++ Assets/Scripts/Networking/NetworkServer.cs
@@ -9,27 +9,73 @@ public class NetworkServer : MonoBehaviour
 	public static bool isMultiplayer = false;
 	public static int NUMBER_OF_CONNECTIONS = 8;
 	
+	[Header("Connection settings")]
+	public static bool startTypeClient = false;
+	
+	//Shared network view for this player/server/whatever
 	private static NetworkView networkView;
 	
+	//List of objects
 	public static List<GameObject> objects = new List<GameObject>();
-	 
+	public static List<NetworkPlayer> players = new List<NetworkPlayer>();
+	
+	public static void connect(string ip, int port, string password)
+	{
+		//A server cannot connect to stuff..
+		if(!startTypeClient)
+			return;
+			
+		//Get the attached network view for RPCs
+		networkView = new NetworkView();
+		
+		//Attempt to connect to the server
+		Network.Connect(ip, port, password);
+	}
+	
+	public void OnConnectedToServer()
+	{
+		Debug.Log ("Connected to server.");
+	}
+	
+	void OnPlayerConnected(NetworkPlayer player) 
+	{
+		if(!isMultiplayer)
+			return;
+		
+		players.Add(player);
+	}
+	
+	void OnPlayerDisconnected(NetworkPlayer player) 
+	{
+		if(!isMultiplayer)
+			return;
+		
+		players.Remove(player);
+	}
+	
 	// Use this for initialization
 	public static void start(int port, string serverPassword) 
 	{
+		//Clients can't start the server..
+		if(startTypeClient)
+			return;
+			
 		//Set up server. First set the incoming password.
 		Network.incomingPassword = serverPassword;
 		
 		//And finally initialize the server
 		Network.InitializeServer(NUMBER_OF_CONNECTIONS, port);
 		
+		//Get network view for script
 		networkView = new NetworkView();
-				
-		DebugLogger.Log ("Initialized server? " + Network.isServer + " .. with password \"" + serverPassword + "\"");
 	} 
 	
 	
 	public static GameObject createObject(Object prefab, Vector3 position, Quaternion rotation)
 	{
+		//Creates an object locally or on the network.
+		//..
+		
 		GameObject addedObject = null;
 		
 		if(isMultiplayer)
